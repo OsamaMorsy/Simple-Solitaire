36d9a68f7dbc488865541ef5972172dd670f1b1b
Osama Morsy <115116181+OsamaMorsy@users.noreply.github.com>
2024-04-24 15:51:25 +0300

    refactoring onCreat function in GameManager.java

--- app/src/main/java/de/tobiasbielefeld/solitaire/ui/GameManager.java
+++ app/src/main/java/de/tobiasbielefeld/solitaire/ui/GameManager.java
@@ -68,17 +68,24 @@ public class GameManager extends AppCompatActivity implements View.OnTouchListen
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_game_manager);
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_game_manager);
 
-        // load stuff
+    initializeUIComponents();
+    initializeGameComponents();
+    initializeCardsAndStacks();
+    postLayoutConfigurations();
+    }
+
+    private void initializeUIComponents() {
         layoutGame = (RelativeLayout) findViewById(R.id.mainRelativeLayoutGame);
         mainTextViewTime = (TextView) findViewById(R.id.mainTextViewTime);
         mainTextViewScore = (TextView) findViewById(R.id.mainTextViewScore);
         mainTextViewRedeals = (TextView) findViewById(R.id.textViewRedeals);
         buttonAutoComplete = (Button) findViewById(R.id.buttonMainAutoComplete);
+    }
 
-        //initialize my static helper stuff
+    private void initializeGameComponents() {
         final GameManager gm = this;
         recordList = new RecordList(gm);
         movingCards = new MovingCards();
@@ -88,23 +95,22 @@ public class GameManager extends AppCompatActivity implements View.OnTouchListen
         animate = new Animate(gm);
         autoComplete = new AutoComplete(gm);
         timer = new Timer(gm);
-        currentGame = lg.loadClass(this,getIntent().getIntExtra("game",1));
+        currentGame = lg.loadClass(this, getIntent().getIntExtra("game", 1));
         savedGameData = getSharedPreferences(lg.getSharedPrefName(), MODE_PRIVATE);
-
-
-        if (savedSharedData==null) {
+    
+        if (savedSharedData == null) {
             savedSharedData = PreferenceManager.getDefaultSharedPreferences(this);
         }
-
-        //initialize cards and stacks
+    }
+    
+    private void initializeCardsAndStacks() {
         for (int i = 0; i < stacks.length; i++) {
             stacks[i] = new Stack(i);
             stacks[i].view = new ImageView(this);
             stacks[i].view.setBackgroundResource(R.drawable.background_stack);
             layoutGame.addView(stacks[i].view);
         }
-        //mainTextViewRedeals.bringToFront();
-
+    
         for (int i = 0; i < cards.length; i++) {
             cards[i] = new Card(i);
             cards[i].view = new ImageView(this);
@@ -112,80 +118,62 @@ public class GameManager extends AppCompatActivity implements View.OnTouchListen
             cards[i].view.setOnTouchListener(this);
             layoutGame.addView(cards[i].view);
         }
-
-
+    
         currentGame.addOnTouchListener(this);
-
-
-        scores.output();
-
-        //post, because i need the dimensions of layoutGame to set the cards and stacks
-        layoutGame.post(new Runnable() {                                                           //post a runnable to set the dimensions of cards and stacks when the layout has loaded
+    }
+    
+    private void postLayoutConfigurations() {
+        layoutGame.post(new Runnable() {
             @Override
             public void run() {
-
-                boolean isLandscape = getResources().getConfiguration().orientation
-                        == Configuration.ORIENTATION_LANDSCAPE;
-
-                currentGame.setStacks(layoutGame, isLandscape);
-
-                //if left handed mode is true, mirror all stacks
-                if (getSharedBoolean(getString(R.string.pref_key_left_handed_mode), false)) {
-                    for (Stack stack : stacks)
-                        stack.view.setX(layoutGame.getWidth() - stack.view.getX() - Card.width);
-
-                    if (currentGame.hasArrow()){
-                        for (Stack stack : stacks){
-                            if (stack.hasArrow()>0) {
-                                if (stack.hasArrow() == 1) {
-                                    stack.view.setBackgroundResource(R.drawable.arrow_right);
-                                } else {
-                                    stack.view.setBackgroundResource(R.drawable.arrow_left);
-                                }
-                            }
-                        }
-                    }
-                }
-
-                //calculate the spacing for cards on a stack
-                Stack.defaultSpacing = Card.width / 2;
-
-                //setup how the cards on the stacks will be stacked (offset to the previous card)
-                //there are 4 possible directions. By default, the tableau stacks are stacked down
-                //all other stacks don't have a visible offset
-                //use setDirections() in a game to change that
-                if (currentGame.directions==null) {
-                    for (int i = 0; i <= currentGame.getLastTableauID(); i++) {
-                        stacks[i].setSpacingDirection(1);
-                    }
-                }
-                else {
-                    for (int i=0;i<currentGame.directions.length;i++) {
-                        stacks[i].setSpacingDirection(currentGame.directions[i]);
-                    }
-                }
-                //if there are direction borders set (when cards should'nt overlap another stack)  use it.
-                //else set the layout height/widht as maximum
-                if (currentGame.directionBorders!=null){
-                    for (int i=0;i<currentGame.directionBorders.length;i++){
-                        if (currentGame.directionBorders[i]!=-1)    //-1 means no border
-                            stacks[i].setSpacingMax(currentGame.directionBorders[i]);
-                        else
-                            stacks[i].setSpacingMax(layoutGame);
-                    }
-                } else {
-                    for (Stack stack : stacks){
-                        stack.setSpacingMax(layoutGame);
-                    }
-                }
-
-                //load the game
+                handleOrientationAndSpacing();
                 scores.load();
                 LoadGameHandler loadGameHandler = new LoadGameHandler(gm);
-                loadGameHandler.sendEmptyMessageDelayed(0,200);
+                loadGameHandler.sendEmptyMessageDelayed(0, 200);
             }
         });
     }
+    
+    private void handleOrientationAndSpacing() {
+        boolean isLandscape = getResources().getConfiguration().orientation == Configuration.ORIENTATION_LANDSCAPE;
+        currentGame.setStacks(layoutGame, isLandscape);
+    
+        if (getSharedBoolean(getString(R.string.pref_key_left_handed_mode), false)) {
+            for (Stack stack : stacks) {
+                stack.view.setX(layoutGame.getWidth() - stack.view.getX() - Card.width);
+                if (currentGame.hasArrow()) {
+                    if (stack.hasArrow() > 0) {
+                        stack.view.setBackgroundResource(stack.hasArrow() == 1 ? R.drawable.arrow_right : R.drawable.arrow_left);
+                    }
+                }
+            }
+        }
+    
+        Stack.defaultSpacing = Card.width / 2;
+        if (currentGame.directions == null) {
+            for (int i = 0; i <= currentGame.getLastTableauID(); i++) {
+                stacks[i].setSpacingDirection(1);
+            }
+        } else {
+            for (int i = 0; i < currentGame.directions.length; i++) {
+                stacks[i].setSpacingDirection(currentGame.directions[i]);
+            }
+        }
+    
+        if (currentGame.directionBorders != null) {
+            for (int i = 0; i < currentGame.directionBorders.length; i++) {
+                if (currentGame.directionBorders[i] != -1)
+                    stacks[i].setSpacingMax(currentGame.directionBorders[i]);
+                else
+                    stacks[i].setSpacingMax(layoutGame);
+            }
+        } else {
+            for (Stack stack : stacks) {
+                stack.setSpacingMax(layoutGame);
+            }
+        }
+    }
+
 
 
 
